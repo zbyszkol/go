98c595d1e899964e682e627bce54420281952f93
Scott Fleckenstein <nullstyle@gmail.com>
2017-08-01 22:53:31 -0800

    Add txresult fuzz test

---
+++ test/fuzz/txresult/main.go
@@ -0,0 +1,32 @@
+// +build gofuzz
+
+package txresult
+
+import (
+	"github.com/stellar/go/xdr"
+	"bytes"
+	"fmt"
+)
+
+func Fuzz(data []byte) int {
+	var txr xdr.TransactionResult
+	err := xdr.SafeUnmarshal(data, &txr)
+	if err != nil {
+		return 0
+	}
+
+	var out bytes.Buffer
+	_, err = xdr.Marshal(&out, txr)
+	if err != nil {
+		fmt.Println("failed to encode")
+		fmt.Printf("%#v", data)
+	}
+
+	if !bytes.Equal(data, out.Bytes()) {
+		fmt.Println("failed to roundtrip")
+		fmt.Printf("%#v", data)
+		panic("failed to roundtrip")
+	}
+
+	return 1
+}
