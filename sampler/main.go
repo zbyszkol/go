78b25802ebf100b79da51db57e6d410642e512f0
Lukas Lachowski <lukasz@stellar.org>
2017-09-19 16:48:43 +0000

    Version passing build tests.

--- sampler/main.go
+++ sampler/main.go
@@ -1,6 +1,7 @@
 package sampler
 
 import (
+	"github.com/stellar/go/xdr"
 	"math/rand"
 )
 
@@ -19,16 +20,25 @@ func SamplerLoop() {
 		}
 
 		database.BeginTransaction()
-		size := Size(rand.Int(100) + 1)
+
+		size := Size(rand.Intn(100) + 1)
 		data, sourceAccount := sampler(size, database)
 		submitResult, seqenceUpdate, transactionResult := submitter.Submit(sourceAccount, data)
 		if submitResult.Err != nil {
-			// TODO scream and run away, don't forget to take dog with you
+			// TODO scream and run away, don't forget to take your dog
 			database.RejectTransaction()
 			continue
 		}
 		newSequenceNum, seqError := seqenceUpdate()
-		sourceAccount.SeqNum = newSequenceNum
+		if seqError != nil {
+			// TODO clean up - there are at least two sampler.go files
+			coreResult, _ := transactionResult()
+			// if resultError != nil {
+			// }
+			database = ApplyChanges(&coreResult.ResultMeta, database)
+		}
+		sourceAccount.SeqNum = xdr.SequenceNumber(newSequenceNum.Sequence)
+
 		database.EndTransaction()
 	}
 }
